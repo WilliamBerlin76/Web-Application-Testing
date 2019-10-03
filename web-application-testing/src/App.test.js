24d45e172a0f1ca847cf6f61989fefabb7221b2f
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-03 16:49:10 -0500

    added another function to a test

--- web-application-testing/src/App.test.js
+++ web-application-testing/src/App.test.js
@@ -7,7 +7,9 @@ test('renders without crashing', () => {
 });
 
 test('addStrike adds one strike to the current strike count, and sets strike to 0 at 3rd strike', () => {
-  expect(addStrike(2)).toBe(0)
+  expect(addStrike(2)).toBe(0);
+  expect(addStrike(1)).toBe(2);
+  expect(addStrike(2)).not.toBe(1);
 })
 
 test('at bat section is found', () => {
